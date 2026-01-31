177911170bcbf0707abc56449a2c608c5acd54e6
Aryan <aryansoy3@gmail.com>
2026-01-31 19:09:08 +0530

    Added UI Scenes

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,12 +1,14 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections;   
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private int maxHealth;
     public int cur_health;
+    private float cur_time = 0f;
+    private Coroutine healthCoroutine;
     private void Awake()
     {
         slider.maxValue = maxHealth;
@@ -22,4 +24,16 @@ public class HealthBar : MonoBehaviour
     {
         slider.value += health;
     }
+
+    public void Update()
+    {
+        if(Time.time - cur_time >= 1)
+        {
+            DecrementHealth(1);
+            cur_time = Time.time;
+        }
+
+    }
+
 }
+
--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -1,31 +1,41 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_Controller : MonoBehaviour
 {
+
+    public void onStartBtn()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
     public void onDisplayBtn()
     {
+        SceneManager.LoadScene("DisplayScreen");
 
     }
 
     public void OnAboutBtn()
     {
-
+        SceneManager.LoadScene("AboutUsScreen");
     }
 
     public void onControlsBtn()
     {
-
+        SceneManager.LoadScene("ControlsScreen");
     }
 
     public void BackToTitle()
     {
-
+        SceneManager.LoadScene("TitleScreen");
     }
 
     public void BackToOptions()
     {
-
+        SceneManager.LoadScene("OptionsScreen");
     }
+
+
 
 
     public void toggleMusic(bool toggle)
@@ -38,5 +48,14 @@ public class UI_Controller : MonoBehaviour
         {
             Debug.Log("Music Disabled");
         }
+    }
+
+    public void onExitBtn()
+    {
+    #if (UNITY_EDITOR)
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
     }
 }
